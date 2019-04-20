b6b6c1affebac5d194fd011e15ab1fce161e4d00
evapphilips <ep1521@nyu.edu>
2019-04-20 13:23:52 -0400

    adding view controller files

---
+++ theZone/StopViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StopViewController.swift
+//  theZone
+//
+//  Created by Eva Philips on 4/20/19.
+//  Copyright © 2019 evaphilips. All rights reserved.
+//
+
+import UIKit
+
+class StopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
